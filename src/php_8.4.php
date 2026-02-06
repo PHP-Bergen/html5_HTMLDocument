2237a3b71871dee2aee2c4c15f4e755658365973
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2026-02-06 23:34:32 +0100

    DOM HTML5 example

--- src/php_8.4.php
+++ src/php_8.4.php
@@ -1,15 +1,7 @@
 <?php
 
 /**
- * PHP 8.4 and later. https://www.php.net/releases/8.4/en.php.
- *
- * Lexbor, written in C. https://lexbor.com/
- * HTML5 support, CSS selectors, and modern DOM features to PHP.
- *
- * https://wiki.php.net/rfc/domdocument_html5_parser
- * https://wiki.php.net/rfc/dom_additions_84
- * https://blog.keyvan.net/p/parsing-html-with-php-84
- * https://blog.keyvan.net/p/interview-with-niels-dossche
+ * DOM HTML5 parsing. PHP 8.4 and newer.
  */
 
 use DOM\HTMLDocument;
