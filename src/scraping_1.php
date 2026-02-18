2b7c3cd447a9aaf6cb8103a7b6eb9762a0e4684a
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2026-02-18 17:55:56 +0100

    update scraping

--- src/scraping_1.php
+++ src/scraping_1.php
@@ -1,14 +1,14 @@
 <?php
 
 $html = file_get_contents('https://www.nrk.no/stor-oslo/ruters-egne-tester-viser_-oslos-elbusser-kan-fjernstyres-1.17629321');
-$nrk = @Dom\HTMLDocument::createFromString($html);
+$content = @Dom\HTMLDocument::createFromString($html);
 
-$elements = $nrk->querySelectorAll('p, h1, h2, h3');
+$elements = $content->querySelectorAll('p, h1, h2, h3');
 foreach ($elements as $element) {
     echo $element->textContent . "\n";
 }
 
-$elements = $nrk->querySelector('article')->querySelectorAll('h1, h2');
+$elements = $content->querySelector('article')->querySelectorAll('h1, h2');
 foreach ($elements as $element) {
     echo $element->textContent . "\n";
 }
